56249deaabcd850bbd93ca66d5ba4208725691e4
HJKim95 <25ephipany@naver.com>
2020-09-04 00:55:26 +0900

    Initial Commit

---
+++ HJPortfolio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HJPortfolio
+//
+//  Created by 김희중 on 2020/09/04.
+//  Copyright © 2020 HJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
